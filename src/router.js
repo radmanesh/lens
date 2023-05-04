9072a3acbee4794b54296d1447e17f3cc07e4f6b
Arman Radmanesh <radmanesh@gmail.com>
2023-05-04 16:45:33 +0330

    Fixed router and added new theme provider

--- src/router.js
+++ src/router.js
@@ -1,19 +1,27 @@
 import React from 'react';
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
 
 //main components
 import Study from './study';
 import About from './about';
 import LanguageSelector from './language_selector';
+import { languages } from './utils/i18n';
+import { ltrTheme, rtlTheme } from './utils/theme';
 
 export default function AppRouter() {
+  let {lang } = useParams();
+  const theme = (lang && lang !== '') ? (languages[lang].direction === 'rtl')?rtlTheme:ltrTheme  : ltrTheme ;
+  
   return (
-    <Routes basename="/">
-        <Route exact path="/" element={<About />} />
-        <Route path="/:studyId/:lang" element={<Study />} />
-        <Route path="/:studyId" element={<LanguageSelector />} />
-        <Route path="/about" element={<About />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes basename="/">
+          <Route exact path="/" element={<About />} />
+          <Route path="/:studyId/:lang" element={<Study />} />
+          <Route path="/:studyId" element={<LanguageSelector />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
